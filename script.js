d85e5cc48be0c77443e0d14f0812aa60aa891193
= <adniyayousaf92@gmail.com>
2024-01-18 12:47:54 +0000

    completed

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
 //You can edit ALL of the code here
 function setup() {
   const allEpisodes = getAllEpisodes();
-  makePageForEpisodes(allEpisodes);
+  const episodeCards = makePageForEpisodes(allEpisodes);
+  document.body.append(...episodeCards);
 }
 
 function makePageForEpisodes(episodeList) {
-  const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+  const episodeCards = episodeList.map(e => createEpisodesCard(e));
+  return episodeCards;
+}
+
+function createEpisodesCard(episode) {
+  const rootElem = document.getElementById("root").content.cloneNode(true);
+  const seasonPluEp = "S" + episode.season.toString().padStart(2, "0") + "E" + episode.number.toString().padStart(2, "0");
+
+  rootElem.querySelector("h1").textContent = episode.name + "-" + seasonPluEp;
+  rootElem.querySelector("img").src = episode.image.medium;
+  rootElem.querySelector('p').innerHTML = episode.summary;
+
+  return rootElem;
 }
 
 window.onload = setup;
+
